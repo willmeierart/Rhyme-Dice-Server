b2a5bfa02e55ff6a123ab5458358e5a32a5db541
WILL MEIER <willmeierart@gmail.com>
2017-09-11 14:40:58 -0600

    initial heroku commit

--- migrations/20170910193941_sounds.js
+++ migrations/20170910193941_sounds.js
@@ -1,7 +1,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('sounds', (table)=>{
       table.increments('id').primary()
-      table.text('sound').notNull()
+      table.text('sound').notNull().unique()
   })
 }
 exports.down = function(knex, Promise) {
