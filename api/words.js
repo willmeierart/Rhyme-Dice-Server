fc65d79687b3b691f0c313574651ae8a6b17aed7
WILL MEIER <willmeierart@gmail.com>
2017-09-11 22:07:31 -0600

    just words string array now

--- api/words.js
+++ api/words.js
@@ -5,18 +5,12 @@ const queries = require('../db/queries')
 router.get('/:sound', function(req, res, next) {
   queries.getWords(req.params.sound)
     .then(wds=>{
-      console.log(wds);
       let numbers = []
       const count = wds.length>25 ? 25 : wds.length
       for (let i=0; i<count; i++){
         numbers.push(Math.floor(Math.random()*wds.length))
       }
-      console.log(numbers);
-      const randomSet = numbers.map(num=>{
-        console.log(wds[num]);
-        return wds[num]
-      })
-      console.log(randomSet);
+      const randomSet = numbers.map(num=>wds[num].word)
       return res.json(randomSet)
     })
 })
